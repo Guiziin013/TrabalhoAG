70f43e9240fad302a608cb0f423122fa52ccf125
Danilo Mancini <haomakk.himself@gmail.com>
2022-09-06 08:25:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TrabalhoAG
+ Bacana
---
+++ app/src/main/java/com/example/trabalhoag/quintatela.java
@@ -0,0 +1,14 @@
+package com.example.trabalhoag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class quintatela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quintatela);
+    }
+}
